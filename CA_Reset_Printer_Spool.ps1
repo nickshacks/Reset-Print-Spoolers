edec0802a0c1a979cdb1e2916b02fe5973f11a64
nickshacks <43885575+nickshacks@users.noreply.github.com>
2018-10-05 07:43:36 -0700

    Complete code
    Complete code for resetting print spooler in CA and MI

---
+++ CA_Reset_Printer_Spool.ps1
@@ -0,0 +1,13 @@
+﻿#set your first argument as $computer 
+$computer = "ca-print.bayley.net" 
+
+#Stop the service: 
+Get-WmiObject -Class Win32_Service -Filter 'name="spooler"' -ComputerName $computer | Invoke-WmiMethod -Name StopService | out-null 
+
+#Delete all items in: C:\windows\system32\spool\printers on the remote computer 
+
+#the `$ is the escape charater to make sure powershell sees the $ as a charater rather than a variable 
+Get-ChildItem "\\$computer\C`$\windows\system32\spool\printers" | Remove-Item 
+
+#Start the service 
+Set-Service spooler -ComputerName $computer -status Running
